c3956e2382547af33eae26d103b15b764558a909
isabela <imorais@asert.com.br>
2017-06-22 15:19:05 -0300

    Módulo 6

--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -1,12 +1,16 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'painel',
     templateUrl: './painel.component.html'
 })
-export class PainelComponent {
+export class PainelComponent implements OnInit{
     
     @Input() titulo: string; //a tipagem é opcional
+
+    ngOnInit(){ // executa o conteúdo depois que as propriedades são passadas
+        this.titulo = this.titulo.length>7 ? this.titulo.substr(0,7) + '...' : this.titulo;
+    }
 
 }
